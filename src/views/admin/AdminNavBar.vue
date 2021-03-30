2bf499a593af0ba4b4e806fe0e906d94a5473196
zjh <1366448731@qq.com>
2021-03-31 01:21:52 +0800

    初步完成

--- src/views/admin/AdminNavBar.vue
+++ src/views/admin/AdminNavBar.vue
@@ -3,13 +3,15 @@
     <b-navbar-nav>
       <b-nav-item href="#">广科校园论坛后台管理系统</b-nav-item>
       <b-nav-item-dropdown :text="user" right>
-        <b-dropdown-item href="#">退出</b-dropdown-item>
+        <b-dropdown-item href="#" @click="logout">退出</b-dropdown-item>
       </b-nav-item-dropdown>
     </b-navbar-nav>
   </b-navbar>
 </template>
 
 <script>
+import { logout, } from "network/home";
+
   export default {
     name:"AdminNavBar",
     data() {
@@ -18,7 +20,22 @@
       }
     },
     methods:{
-
+       //退出
+    logout() {
+      console.log("退出");
+      logout().then((res) => {
+        //删除的vuex和localStorage的登录信息
+          this.$store.commit("login", false);
+          this.$store.commit("username",undefined);
+          this.$store.commit("userId",undefined);
+          localStorage.removeItem("isLogin");
+          localStorage.removeItem("username");
+          localStorage.removeItem("userId");
+          localStorage.removeItem("userType");
+          //去首页
+          this.$router.push("/login")
+      });
+    },
     },
     computed:{
       user(){
